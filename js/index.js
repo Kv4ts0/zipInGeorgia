4d340fe349aa85e74664cbde29006f9407000a5d
Nikusha Kvatsabaia <46217588+Kv4ts0@users.noreply.github.com>
2023-12-28 03:04:33 +0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ let prevDom = document.getElementById('prev');
 let carouselDom = document.querySelector('.carousel');
 let listItemDom = document.querySelector('.carousel .list');
 let thumbnailDom = document.querySelector('.carousel .thumbnail');
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
 
 nextDom.onclick = function(){
     showSlider('next');
@@ -41,4 +44,17 @@ function showSlider(type){
     runAutoRun = setTimeout(()=>{
         nextDom.click();
     }, timeAutoNext);
+}
+
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+
+function close(){
+    mainMenu.style.display = 'none';
 }
